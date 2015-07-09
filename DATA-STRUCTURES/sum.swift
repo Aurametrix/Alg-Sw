cd555a56721f2ba068c32997504d51ed7eaf9861
Aurametrix <irene@aurametrix.com>
2015-07-08 22:02:20 -0700

    A + B

--- DATA-STRUCTURES/sum.swift
+++ DATA-STRUCTURES/sum.swift
@@ -4,3 +4,16 @@ println(a.reduce(1, *)) // prints 120
  
 println(reduce(a, 0, +)) // prints 15
 println(reduce(a, 1, *)) // prints 120
+
+import Foundation
+ 
+let input = NSFileHandle.fileHandleWithStandardInput()
+ 
+let data = input.availableData
+let str = NSString(data: data, encoding: NSUTF8StringEncoding)!
+ 
+let nums = str.componentsSeparatedByString(" ")
+let a = (nums[0] as String).toInt()!
+let b = (nums[1] as String).toInt()!
+ 
+print(" \(a + b)")
